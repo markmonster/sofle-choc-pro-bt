da1d56626a06019696e8852dc8301e28d5352ff4
Mark Monster <mark@monsterconsultancy.nl>
2026-05-18 20:31:07 +0200

    🐛 Fix right display logo rendering

--- boards/shields/nice_view_disp/widgets/peripheral_status.c
+++ boards/shields/nice_view_disp/widgets/peripheral_status.c
@@ -122,8 +122,6 @@ int zmk_widget_status_init(struct zmk_widget_status *widget, lv_obj_t *parent) {
 
     lv_obj_t *art = lv_img_create(widget->obj);
     lv_img_set_src(art, &mclogo);
-    lv_img_set_angle(art, 900);
-    lv_img_set_pivot(art, 34, 70);
     lv_obj_set_size(art, 140, 68);
     lv_obj_align(art, LV_ALIGN_TOP_LEFT, art_pos, 0);
 
