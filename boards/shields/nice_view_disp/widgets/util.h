b6b4f5869f7f37c9d7564f3df1c3aaed15a4624d
Mark Monster <mark@monsterconsultancy.nl>
2026-05-26 16:16:51 +0200

    ✨ make quote probe source sized

--- boards/shields/nice_view_disp/widgets/util.h
+++ boards/shields/nice_view_disp/widgets/util.h
@@ -9,10 +9,8 @@
 #include <zmk/endpoints.h>
 
 #define CANVAS_SIZE 68
-#define QUOTE_SRC_CANVAS_WIDTH 160
-#define QUOTE_SRC_CANVAS_HEIGHT 68
-#define QUOTE_CANVAS_WIDTH QUOTE_SRC_CANVAS_HEIGHT
-#define QUOTE_CANVAS_HEIGHT QUOTE_SRC_CANVAS_WIDTH
+#define QUOTE_CANVAS_WIDTH 104
+#define QUOTE_CANVAS_HEIGHT 68
 
 #define LVGL_BACKGROUND                                                                            \
     IS_ENABLED(CONFIG_NICE_VIEW_DISP_WIDGET_INVERTED) ? lv_color_black() : lv_color_white()
