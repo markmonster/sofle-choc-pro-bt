cad257ae90522bea71a70dfa73c65334b98c5640
Mark Monster <mark@monsterconsultancy.nl>
2026-05-30 14:14:41 +0200

    🧪 map quote diagnostic grid

--- boards/shields/nice_view_disp/widgets/status.c
+++ boards/shields/nice_view_disp/widgets/status.c
@@ -102,12 +102,15 @@ static void draw_quote_band(lv_obj_t *widget, lv_color_t cbuf[]) {
     lv_canvas_draw_line(source_canvas, center_vertical, 2, &line_dsc);
     lv_canvas_draw_line(source_canvas, center_horizontal, 2, &line_dsc);
 
-    lv_canvas_draw_text(source_canvas, 1, 4, 20, &label_dsc, "L");
-    lv_canvas_draw_text(source_canvas, 24, 4, 20, &label_dsc, "C");
-    lv_canvas_draw_text(source_canvas, 47, 4, 20, &label_dsc, "R");
-    lv_canvas_draw_text(source_canvas, 1, 84, 20, &label_dsc, "L");
-    lv_canvas_draw_text(source_canvas, 24, 84, 20, &label_dsc, "C");
-    lv_canvas_draw_text(source_canvas, 47, 84, 20, &label_dsc, "R");
+    lv_canvas_draw_text(source_canvas, 0, 4, 16, &label_dsc, "A");
+    lv_canvas_draw_text(source_canvas, 26, 4, 16, &label_dsc, "B");
+    lv_canvas_draw_text(source_canvas, 52, 4, 16, &label_dsc, "C");
+    lv_canvas_draw_text(source_canvas, 0, 44, 16, &label_dsc, "D");
+    lv_canvas_draw_text(source_canvas, 26, 44, 16, &label_dsc, "E");
+    lv_canvas_draw_text(source_canvas, 52, 44, 16, &label_dsc, "F");
+    lv_canvas_draw_text(source_canvas, 0, 84, 16, &label_dsc, "G");
+    lv_canvas_draw_text(source_canvas, 26, 84, 16, &label_dsc, "H");
+    lv_canvas_draw_text(source_canvas, 52, 84, 16, &label_dsc, "I");
 #else
     lv_canvas_draw_text(source_canvas, 0, 6, QUOTE_SOURCE_CANVAS_WIDTH, &label_dsc, "If you");
     lv_canvas_draw_text(source_canvas, 0, 14, QUOTE_SOURCE_CANVAS_WIDTH, &label_dsc, "can't");
