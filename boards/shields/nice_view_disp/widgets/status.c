92ecfd8edb479a86e5557ec550404628afcfab97
Mark Monster <mark@monsterconsultancy.nl>
2026-05-30 14:44:06 +0200

    🧪 mark quote safe text area

--- boards/shields/nice_view_disp/widgets/status.c
+++ boards/shields/nice_view_disp/widgets/status.c
@@ -102,15 +102,20 @@ static void draw_quote_band(lv_obj_t *widget, lv_color_t cbuf[]) {
     lv_canvas_draw_line(source_canvas, center_vertical, 2, &line_dsc);
     lv_canvas_draw_line(source_canvas, center_horizontal, 2, &line_dsc);
 
-    lv_canvas_draw_text(source_canvas, 8, 4, 16, &label_dsc, "A");
-    lv_canvas_draw_text(source_canvas, 26, 4, 16, &label_dsc, "B");
-    lv_canvas_draw_text(source_canvas, 44, 4, 16, &label_dsc, "C");
-    lv_canvas_draw_text(source_canvas, 8, 44, 16, &label_dsc, "D");
-    lv_canvas_draw_text(source_canvas, 26, 44, 16, &label_dsc, "E");
-    lv_canvas_draw_text(source_canvas, 44, 44, 16, &label_dsc, "F");
-    lv_canvas_draw_text(source_canvas, 8, 84, 16, &label_dsc, "G");
-    lv_canvas_draw_text(source_canvas, 26, 84, 16, &label_dsc, "H");
-    lv_canvas_draw_text(source_canvas, 44, 84, 16, &label_dsc, "I");
+    lv_point_t safe_top[] = {{14, 0}, {QUOTE_SOURCE_CANVAS_WIDTH - 1, 0}};
+    lv_point_t safe_left[] = {{14, 0}, {14, QUOTE_SOURCE_CANVAS_HEIGHT - 1}};
+    lv_canvas_draw_line(source_canvas, safe_top, 2, &line_dsc);
+    lv_canvas_draw_line(source_canvas, safe_left, 2, &line_dsc);
+
+    lv_canvas_draw_text(source_canvas, 14, 4, 14, &label_dsc, "A");
+    lv_canvas_draw_text(source_canvas, 32, 4, 14, &label_dsc, "B");
+    lv_canvas_draw_text(source_canvas, 50, 4, 14, &label_dsc, "C");
+    lv_canvas_draw_text(source_canvas, 14, 44, 14, &label_dsc, "D");
+    lv_canvas_draw_text(source_canvas, 32, 44, 14, &label_dsc, "E");
+    lv_canvas_draw_text(source_canvas, 50, 44, 14, &label_dsc, "F");
+    lv_canvas_draw_text(source_canvas, 14, 84, 14, &label_dsc, "G");
+    lv_canvas_draw_text(source_canvas, 32, 84, 14, &label_dsc, "H");
+    lv_canvas_draw_text(source_canvas, 50, 84, 14, &label_dsc, "I");
 #else
     lv_canvas_draw_text(source_canvas, 0, 6, QUOTE_SOURCE_CANVAS_WIDTH, &label_dsc, "If you");
     lv_canvas_draw_text(source_canvas, 0, 14, QUOTE_SOURCE_CANVAS_WIDTH, &label_dsc, "can't");
